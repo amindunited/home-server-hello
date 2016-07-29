cf15d9875d79b90f181411bca0086e89f17ed061
Robin Buckley <buckley.robin@gmail.com>
2016-07-30 00:34:26 +1000

    Create hello world server

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+//import express from 'express';
+var express = require('express');
+var http  = require('http');
+var path = require('path');
+var fs  = require('fs');
+//var ncp  = require('ncp');
+//var glob  = require('glob');
+
+class Server {
+
+  constructor () {
+
+    let app = express();
+
+    app.set('port', 4200);
+
+    app.use(express.static(path.join(__dirname, './dist')));
+
+    http.createServer(app).listen(app.get('port'), function () {
+      console.log('Express server listening on port ' + app.get('port'));
+    });
+
+  }
+
+}
+
+//export default new Server();
+module.exports = new Server();
